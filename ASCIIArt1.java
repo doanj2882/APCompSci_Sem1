cdf5ebeeff53fd8f84e3b59d7d668cad94bf78a7
doanj2882 <doan.jared@yahoo.com>
2016-09-02 13:47:17 -0700

    Added Files

---
+++ ASCIIArt1.java
@@ -0,0 +1,15 @@
+public class firstProg
+{
+	public static void main(String[]args)
+		{
+			System.out.println();
+			System.out.println("hhhhh__ /                      _aaaaaaaaaaaaa              _rrrrr                           _aaaaaaaaaaaaa               mmmm                       bbbbb__/                     __eeeeeeeeeeeee__/");
+			System.out.println("hhhhh__/                    _aaaaaa__/     aaaa          rrrrrrrrrrrrr__/              _aaaaaa__/     aaaa             mmmmmmmm  mmmmm              bbbbb__/                    eeeeeeeeeeeeeeee__/");
+			System.out.println("hhhhh__/     ___         _aaaa__/          aaaaa           rrrrrrrrrrrrrrr_/         _aaaa__/         aaaaa           mmmmmmmmm mmmmmm__/           bbbbb__/                    eeeeee__/     eee__/");
+			System.out.println("hhhhhhhhhhhhh__/         _aaaa__/          aaaaa           rrrrr__/     rr_/         _aaaa__/         aaaaa           mmmm__/  m_ mmmm__/           bbbbbbbbbbbbbbb__/          eeeee__/      eee__/");
+			System.out.println("hhhhhhhhhhhhh__/         _aaaa__/          aaaaa           rrrrr__/                  _aaaa__/         aaaaa           mmmm__/     mmmm__/           bbbbbbbbbbbbbbb__/          eeeeeeeeeeeeeeeee__/");
+			System.out.println("hhhhh__/  hhh__/         _aaaaa__/         aaaaa           rrrrr__/                  _aaaaa__/        aaaaa           mmmm__/     mmmm__/           bbbbb__/   bbbb__/          _eeeee__/");
+			System.out.println("hhhhh__/  hhh__/           _aaaaaa__/      aaaaa           rrrrr__/                    _aaaaaa__/     aaaaa           mmmm__/     mmmm__/           bbbbb__/   bbbb__/          __eeeeee__/   eeee__/");
+			System.out.println("hhhhh__/  hhh__/             _aaaaaa___    aaaaa           rrrrr__/                      _aaaaaa___   aaaaa           mmmm__/     mmmm__/           bbbbbbbbbbbbbbb__/          ___eeeeeeeeeeeeee__/");
+		} 
+}
