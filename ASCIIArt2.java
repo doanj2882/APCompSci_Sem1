59b2ee17965e2316c7f3fd2fa4433e96b12f3324
doanj2882 <doan.jared@yahoo.com>
2016-09-02 14:06:29 -0700

    Added Files Cont.

---
+++ ASCIIArt2.java
@@ -0,0 +1,10 @@
+public class ASCIIArt2
+{
+	public static void main(String[]args)
+		{
+			System.out.println(" JJJJ    AA    RRRb. EEEE DDDb.    DDDb. .dOOb.    db    Nb  N");
+			System.out.println("   J    dAAb   R  .R Ewww D   D    D   D 8O  O8   dAAb   7NbwN");
+			System.out.println("w  J   dAwwAb  RwwR' E    D   D    D   D 0b  d0  dAwwAb  7  ^N");
+			System.out.println("`Jw^  dA    Ab R  Rb EEEE DDDP'    DDDP' `YOOP' dA    Ab N   N ");
+		}
+}
