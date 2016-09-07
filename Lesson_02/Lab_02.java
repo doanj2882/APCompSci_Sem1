cdbe53c8b7b4f3d608e8b34a2deb523b21bd7714
doanj2882 <DoanJ2882@sduhsd.lan>
2016-09-07 13:50:09 -0700

    completed lab 2

---
+++ Lesson_02/Lab_02.java
@@ -0,0 +1,21 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("2 multiplied by 4 is " + 2*4);
+		
+		String first = "Jared Doan";
+		System.out.println(first);
+		String second = "1234 NotAFake Road";
+		System.out.println(second);
+		String third = "San Diego";
+		System.out.println(third);
+		String fourth = "92130";
+		System.out.println(fourth);
+		
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		System.out.println("The surface area of your reactangle is " + 2*((w * l) + (h * l) + (h * w)));
+	}
+}
