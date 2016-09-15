6b31c3e96a490660b845079e4edb79a05e2906f6
doanj2882 <DoanJ2882@sduhsd.lan>
2016-09-15 14:34:05 -0700

    revised

---
+++ Lesson_02/Lab_02_Revised.java
@@ -0,0 +1,23 @@
+public class Lab_02_Revised
+{
+	public static void main(String[]args)
+	{
+		int x = 10;
+		int y = 99;
+		System.out.println("x multiplied by y is " + x*y);
+		
+		String first = "Jared Doan";
+		System.out.println(first);
+		String second = "1234 NotAFake Road";
+		System.out.println(second);
+		String third = "San Diego";
+		System.out.println(third);
+		String fourth = "92130";
+		System.out.println(fourth);
+		
+		int l = 3;
+		int w = 4;
+		int h = 5;
+		System.out.println("The surface area of your reactangle is " + 2*((w * l) + (h * l) + (h * w)));
+	}
+}
