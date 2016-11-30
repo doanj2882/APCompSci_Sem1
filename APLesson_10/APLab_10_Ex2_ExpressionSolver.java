d61e19bd9689d94b5011d6ace227c0d7a31a1b3e
doanj2882 <DoanJ2882@sduhsd.lan>
2016-11-30 13:29:07 -0800

    11.30.16

--- APLesson_10/APLab_10_Ex2_ExpressionSolver.java
+++ APLesson_10/APLab_10_Ex2_ExpressionSolver.java
@@ -15,6 +15,25 @@ public class APLab_10_Ex2_ExpressionSolver
 	}	
 	public static ArrayList<String> doEquation(ArrayList <String> eqn)	
 	{	
+		int j = 0;
+		while(j < eqn.size())
+		{
+			if(j < eqn.size() && (eqn.get(j).equals("*") || eqn.get(j).equals("/")))
+			{
+				if (eqn.get(j).equals("*"))
+				{	
+					eqn.set(j, "" + (Integer.parseInt(eqn.get(j-1))*Integer.parseInt(eqn.get(j+1))));
+				}
+				else
+				{
+					eqn.set(j, "" + (Integer.parseInt(eqn.get(j-1))/Integer.parseInt(eqn.get(j+1))));
+				}
+				eqn.remove(j-1);
+				eqn.remove(j);
+			}
+			else
+				j++;	
+		}
 		int i = 0;
 		while(i < eqn.size())
 		{
@@ -27,25 +46,6 @@ public class APLab_10_Ex2_ExpressionSolver
 				else
 				{
 					eqn.set(i, "" + (Integer.parseInt(eqn.get(i-1)) - Integer.parseInt(eqn.get(i+1))));
-				}
-				eqn.remove(i-1);
-				eqn.remove(i);
-			}
-			else
-				i++;	
-		}
-		
-		while(i < eqn.size())
-		{
-			if(i < eqn.size() && (eqn.get(i).equals("*") || eqn.get(i).equals("/")))
-			{
-				if (eqn.get(i).equals("*"))
-				{	
-					eqn.set(i, "" + (Integer.parseInt(eqn.get(i-1)) * Integer.parseInt(eqn.get(i+1))));
-				}
-				else
-				{
-					eqn.set(i, "" + (Integer.parseInt(eqn.get(i-1)) / Integer.parseInt(eqn.get(i+1))));
 				}
 				eqn.remove(i-1);
 				eqn.remove(i);
